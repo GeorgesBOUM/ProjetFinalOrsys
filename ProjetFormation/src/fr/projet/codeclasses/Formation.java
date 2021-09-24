ff78e789bd4c905146ef74f5a0e48b7349aa9842
GeorgesBOUM <boumgeorges@protonmail.com>
2021-09-24 12:25:29 +0200

    ajout de la classe Formation

---
+++ ProjetFormation/src/fr/projet/codeclasses/Formation.java
@@ -0,0 +1,56 @@
+package fr.projet.codeclasses;
+
+public class Formation {
+	
+	private String titre;
+	private Formateur formateur;
+	private double prix;
+	private int nbreHeures;
+	
+	public Formation(String titre, Formateur formateur, double prix, int nbreHeures) {
+		this.titre = titre;
+		this.formateur = formateur;
+		this.prix = prix;
+		this.nbreHeures = nbreHeures;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public Formateur getFormateur() {
+		return formateur;
+	}
+
+	public void setFormateur(Formateur formateur) {
+		this.formateur = formateur;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getNbreHeures() {
+		return nbreHeures;
+	}
+
+	public void setNbreHeures(int nbreHeures) {
+		this.nbreHeures = nbreHeures;
+	}
+
+	@Override
+	public String toString() {
+		return "Formation [titre=" + titre + ", formateur=" + formateur + ", prix=" + prix + ", nbreHeures="
+				+ nbreHeures + "]";
+	}
+	
+	
+}
