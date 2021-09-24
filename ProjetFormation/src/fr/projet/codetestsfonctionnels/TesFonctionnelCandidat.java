bdcee053d037656a77282f47af3cd6ef63ede53c
GeorgesBOUM <boumgeorges@protonmail.com>
2021-09-24 13:23:30 +0200

    màj test fonctionnel Candidat

--- ProjetFormation/src/fr/projet/codetestsfonctionnels/TesFonctionnelCandidat.java
+++ ProjetFormation/src/fr/projet/codetestsfonctionnels/TesFonctionnelCandidat.java
@@ -7,8 +7,21 @@ public class TesFonctionnelCandidat {
 	public static void main(String[] args) {
 		try {
 			Candidat candidatUn = new Candidat(0, "nom", "departement");
+			System.out.println(candidatUn);
+		} catch (IllegalArgumentException e) {
+			System.out.println("id <= 0");
+		}
+		
+		try {
 			Candidat candidatDeux = new Candidat(-3, "nom", "departement");
+			System.out.println(candidatDeux);
+		} catch (IllegalArgumentException e) {
+			System.out.println("id <= 0");
+		}
+		
+		try {
 			Candidat candidatTrois = new Candidat(10, "nom", "departement");
+			System.out.println(candidatTrois);
 		} catch (IllegalArgumentException e) {
 			System.out.println("id <= 0");
 		}
