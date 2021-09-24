87986bd76dcf33a33a34b18dde24109d02190760
GeorgesBOUM <boumgeorges@protonmail.com>
2021-09-24 14:31:05 +0200

    finalisation tests du composant Formation

--- ProjetFormation/src/fr/projet/test/TestFormation.java
+++ ProjetFormation/src/fr/projet/test/TestFormation.java
@@ -1,35 +1,42 @@
 package fr.projet.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import fr.projet.codeclasses.Formateur;
+import fr.projet.codeclasses.Formateur.Grade;
+import fr.projet.codeclasses.Formation;
+
 public class TestFormation {
-	private Formation f;
+	private Formation f1;
+	private Formation f2;
+	private Formation f3;
+	private Formateur formateur;
 
 	@Before
 	public void setUp() throws Exception {
-		f = new Formation();
+		formateur = new Formateur("nom", "prenom", "specialite", Grade.JUNIOR);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void testLongueurTitre() {
-		assertTrue("Titre ayant plus de 3 caractères", f.getTitre.length() > 3);
+		f1 = new Formation("ti", formateur, 200, 20);
 	}
 	
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void testPrix() {
-		assertTrue("prix supérieur à 100", f.getPrix() > 100);
+		f2 = new Formation("titre", formateur, 99, 12);
 	}
 	
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void testNbHeures() {
-		assertTrue("nbre heure supérieur à 10", f.getNbreHeures() > 10);
+		f3 = new Formation("titre", formateur, 200, 9);
 	}
 }
